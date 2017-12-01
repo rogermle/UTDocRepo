b1b84a33c87725ce4bafd92b2776437fccbad81c
Roger Le <roger.le@austin.utexas.edu>
2017-11-30 18:42:21 -0600

    Adding config.ini for Travis CI builds

--- tests/test_bootstrap.php
+++ tests/test_bootstrap.php
@@ -7,6 +7,6 @@ require_once 'vendor/autoload.php';
 
 set_include_path(realpath(dirname(dirname(__FILE__))) . PATH_SEPARATOR . get_include_path());
 
-define('CONFIG_FILE', realpath(dirname(__FILE__)) . '/' . 'config.ini');
+define('CONFIG_FILE', realpath(dirname(__FILE__)) . '/' . 'config.ini.defaults');
 define('TEST_FILE', realpath(dirname(__FILE__)) . '/' . 'test_file.txt');
 define('TEST_FILE_MODIFIED', realpath(dirname(__FILE__)) . '/' . 'test_file_modified.txt');
